d970129be9f0d204588584a78ce38cfc2038a379
Mike Kasberg <kasberg.mike@gmail.com>
2024-12-29 15:40:08 -0700

    Passwordless sudo on GH

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -3,11 +3,12 @@
 ZIG_VERSION="0.13.0"
 
 # Build Tools
-apt-get install -y build-essential debhelper pandoc
+sudo apt-get install -y build-essential debhelper pandoc
 
 curl "https://ziglang.org/download/$ZIG_VERSION/zig-linux-x86_64-$ZIG_VERSION.tar.xz" -o "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-tar -xf "zig-linux-x86_64-$ZIG_VERSION.tar.xz" -C /opt
-ln -s "/opt/zig-linux-x86_64-$ZIG_VERSION/zig" /usr/local/bin/zig
+sudo tar -xf "zig-linux-x86_64-$ZIG_VERSION.tar.xz" -C /opt
+rm "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
+sudo ln -s "/opt/zig-linux-x86_64-$ZIG_VERSION/zig" /usr/local/bin/zig
 
 # Ghostty Dependencies
-apt-get install -y libgtk-4-dev libadwaita-1-dev
+sudo apt-get install -y libgtk-4-dev libadwaita-1-dev
