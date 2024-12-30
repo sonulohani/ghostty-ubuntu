49a243871efc27d5e8dbb7cc93c80b8d622ab58f
Mike Kasberg <kasberg.mike@gmail.com>
2024-12-30 08:35:21 -0700

    No sudo inside container

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -3,12 +3,12 @@
 ZIG_VERSION="0.13.0"
 
 # Build Tools
-sudo apt-get update && sudo apt-get install -y build-essential debhelper devscripts pandoc libonig-dev libbz2-dev
+apt-get update && sudo apt-get install -y build-essential debhelper devscripts pandoc libonig-dev libbz2-dev
 
 curl -s "https://ziglang.org/download/$ZIG_VERSION/zig-linux-x86_64-$ZIG_VERSION.tar.xz" -o "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-sudo tar -xf "zig-linux-x86_64-$ZIG_VERSION.tar.xz" -C /opt
+tar -xf "zig-linux-x86_64-$ZIG_VERSION.tar.xz" -C /opt
 rm "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-sudo ln -s "/opt/zig-linux-x86_64-$ZIG_VERSION/zig" /usr/local/bin/zig
+ln -s "/opt/zig-linux-x86_64-$ZIG_VERSION/zig" /usr/local/bin/zig
 
 # Ghostty Dependencies
-sudo apt-get install -y libgtk-4-dev libadwaita-1-dev
+apt-get install -y libgtk-4-dev libadwaita-1-dev
