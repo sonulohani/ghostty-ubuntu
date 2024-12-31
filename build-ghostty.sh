01f036654c9627254a8d0442de6c14a351bdf49f
Mike Kasberg <kasberg.mike@gmail.com>
2024-12-31 14:46:13 -0700

    Update for Ghostty 1.0.1

--- build-ghostty.sh
+++ build-ghostty.sh
@@ -3,7 +3,7 @@
 set -e
 
 ZIG_VERSION="0.13.0"
-GHOSTTY_VERSION="1.0.0"
+GHOSTTY_VERSION="1.0.1"
 
 export DEBEMAIL="kasberg.mike@gmail.com"
 export DEBFULLNAME="Mike Kasberg"
@@ -28,15 +28,13 @@ ln -s "/opt/zig-linux-x86_64-$ZIG_VERSION/zig" /usr/local/bin/zig
 apt-get -qq install -y libgtk-4-dev libadwaita-1-dev
 
 # Fetch Ghostty Source
-wget -q "https://release.files.ghostty.org/$GHOSTTY_VERSION/ghostty-source.tar.gz"
-wget -q "https://release.files.ghostty.org/$GHOSTTY_VERSION/ghostty-source.tar.gz.minisig"
+wget -q "https://release.files.ghostty.org/$GHOSTTY_VERSION/ghostty-$GHOSTTY_VERSION.tar.gz"
+wget -q "https://release.files.ghostty.org/$GHOSTTY_VERSION/ghostty-$GHOSTTY_VERSION.tar.gz.minisig"
 
-minisign -Vm "ghostty-source.tar.gz" -P RWQlAjJC23149WL2sEpT/l0QKy7hMIFhYdQOFy0Z7z7PbneUgvlsnYcV
-rm ghostty-source.tar.gz.minisig
-mv ghostty-source.tar.gz "ghostty-$GHOSTTY_VERSION.tar.gz"
+minisign -Vm "ghostty-$GHOSTTY_VERSION.tar.gz" -P RWQlAjJC23149WL2sEpT/l0QKy7hMIFhYdQOFy0Z7z7PbneUgvlsnYcV
+rm ghostty-$GHOSTTY_VERSION.tar.gz.minisig
 
 tar -xzmf "ghostty-$GHOSTTY_VERSION.tar.gz"
-mv ghostty-source "ghostty-$GHOSTTY_VERSION"
 ln -s "ghostty-$GHOSTTY_VERSION.tar.gz" "ghostty_$GHOSTTY_VERSION.orig.tar.gz"
 
 cp -r debian "ghostty-$GHOSTTY_VERSION/debian"
