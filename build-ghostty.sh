fd5fc44d202ad1e9beef7caeedb1d12da167ea97
Mike Kasberg <kasberg.mike@gmail.com>
2024-12-31 18:39:38 -0700

    Separate env setup script
    I'm on the fence about whether it would be easier to just maintain a
    Dockerfile for the build env, but I think moving env setup to a separate
    script is a step in the right direction either way.

--- build-ghostty.sh
+++ build-ghostty.sh
@@ -2,30 +2,13 @@
 
 set -e
 
-ZIG_VERSION="0.13.0"
 GHOSTTY_VERSION="1.0.1"
 
-export DEBEMAIL="kasberg.mike@gmail.com"
-export DEBFULLNAME="Mike Kasberg"
-export DEBUILD_DPKG_BUILDPACKAGE_OPTS="-i -I -us -uc"
-export DEBUILD_LINTIAN_OPTS="-i -I --show-overrides"
-export DEB_BUILD_MAINT_OPTIONS="hardening=+all"
-
-# Install Build Tools
-apt-get -qq update && apt-get -qq install -y build-essential debhelper devscripts pandoc libonig-dev libbz2-dev wget
-
-wget -q "https://github.com/jedisct1/minisign/releases/download/0.11/minisign-0.11-linux.tar.gz"
-tar -xzf minisign-0.11-linux.tar.gz
-mv minisign-linux/x86_64/minisign /usr/local/bin
-rm -r minisign-linux
-
-wget -q "https://ziglang.org/download/$ZIG_VERSION/zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-tar -xf "zig-linux-x86_64-$ZIG_VERSION.tar.xz" -C /opt
-rm "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-ln -s "/opt/zig-linux-x86_64-$ZIG_VERSION/zig" /usr/local/bin/zig
-
-# Install Ghostty Dependencies
-apt-get -qq install -y libgtk-4-dev libadwaita-1-dev
+DEBEMAIL="kasberg.mike@gmail.com"
+DEBFULLNAME="Mike Kasberg"
+DEBUILD_DPKG_BUILDPACKAGE_OPTS="-i -I -us -uc"
+DEBUILD_LINTIAN_OPTS="-i -I --show-overrides"
+DEB_BUILD_MAINT_OPTIONS="hardening=+all"
 
 # Fetch Ghostty Source
 wget -q "https://release.files.ghostty.org/$GHOSTTY_VERSION/ghostty-$GHOSTTY_VERSION.tar.gz"
---
+++ setup-env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Sets up the environment to build Ghostty on Ubuntu
+
+set -e
+
+ZIG_VERSION="0.13.0"
+
+# Install Build Tools
+apt-get -qq update && apt-get -qq install -y build-essential debhelper devscripts pandoc libonig-dev libbz2-dev wget
+
+wget -q "https://github.com/jedisct1/minisign/releases/download/0.11/minisign-0.11-linux.tar.gz"
+tar -xzf minisign-0.11-linux.tar.gz
+mv minisign-linux/x86_64/minisign /usr/local/bin
+rm -r minisign-linux
+
+wget -q "https://ziglang.org/download/$ZIG_VERSION/zig-linux-x86_64-$ZIG_VERSION.tar.xz"
+tar -xf "zig-linux-x86_64-$ZIG_VERSION.tar.xz" -C /opt
+rm "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
+ln -s "/opt/zig-linux-x86_64-$ZIG_VERSION/zig" /usr/local/bin/zig
+
+# Install Ghostty Dependencies
+apt-get -qq install -y libgtk-4-dev libadwaita-1-dev
